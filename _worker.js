8adef39429f057389f61afc6fc91cfee9defc36c
yd072 <luiquirong@gmail.com>
2025-07-16 18:49:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3127,7 +3127,7 @@ async function handleGetRequest(env, txt) {
                             <textarea 
                                 id="nat64" 
                                 class="proxyip-editor" 
-                                placeholder="${decodeURIComponent(atob('JUU0JUJFJThCJUU1JUE2JTgyJTIwJTBBZG5zNjQuZXhhbXBsZS5jb20lMEEyYTAxJTNBNGY4JTNBYzJjJTNBMTIzZiUzQSUzQTE='))}"
+                                placeholder="${decodeURIComponent(atob('JUU0JUJFJThCJUU1JUE2JTgyJTIwZGVuczY0LmV4YW1wbGUuY29tJTAKMmEwMTolM0E0ZjglM0FjMmMlM0ExMjNmJTNBJTNBJTJGMQ=='))}"
                             >${nat64Content}</textarea>
                         </div>
 
