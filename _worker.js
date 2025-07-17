9835f4030a00c72438470f12b6c6ac88128e0682
yd072 <luiquirong@gmail.com>
2025-07-17 20:04:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1964,10 +1964,10 @@ async function 生成配置信息(userID, hostName, sub, UA, RproxyIP, _url, fak
 	}
 
 	if ((addresses.length + addressesapi.length + addressesnotls.length + addressesnotlsapi.length + addressescsv.length) == 0) {
-	    let cfips = [
+	    	let cfips = [
         		'104.16.0.0/14',
+		        '104.21.0.0/16',
 		        '188.114.96.0/20',
-			'104.21.0.0/16',
 
     			];
 
