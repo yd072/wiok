7c43cc38438e492f984dc2668d8cf943fbf4bce7
yd072 <luiquirong@gmail.com>
2025-07-24 15:23:34 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1139,7 +1139,7 @@ async function handleTCPOutBound(remoteSocket, addressType, addressRemote, portR
 					name: '内置的默认 PROXYIP',
 					enabled: true,
 					execute: async () => {
-						const defaultProxyIP = kodi.tv;
+						const defaultProxyIP = atob('UFJPWFlJUC50cDEuZnh4ay5kZWR5bi5pbw==');
 						const { address, port } = parseProxyIP(defaultProxyIP, portRemote);
 						return createConnection(address, port);
 					}
