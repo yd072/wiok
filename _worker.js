5445c6fa5181548fea1c081b0aa9bb42c43ee60e
yd072 <luiquirong@gmail.com>
2025-07-26 01:53:47 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1122,6 +1122,7 @@ async function handleTCPOutBound(remoteSocket, addressType, addressRemote, portR
 		},
 		{
 			name: '内置的默认 PROXYIP',
+			enabled: true,
 			execute: async () => {
 				const defaultProxyIP = atob('UFJPWFlJUC50cDEuZnh4ay5kZWR5bi5pbw==');
 				const { address, port } = parseProxyIP(defaultProxyIP, portRemote);
