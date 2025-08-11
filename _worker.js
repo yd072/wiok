e40112b3e9ae94dc15726cc910fce4397fb9cea8
yd072 <luiquirong@gmail.com>
2025-08-11 21:04:52 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -8,7 +8,7 @@ let cachedSettings = null;       // 用于存储从KV读取的配置对象
 let userID = '';
 let proxyIP = '';
 //let sub = '';
-let subConverter = '';
+let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
 let subConfig = atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FDTDRTU1IvQUNMNFNTUi9tYXN0ZXIvQ2xhc2gvY29uZmlnL0FDTDRTU1JfT25saW5lX01pbmlfTXVsdGlNb2RlLmluaQ==');
 let subProtocol = 'https';
 let subEmoji = 'true';
@@ -2747,7 +2747,7 @@ function generateSingboxConfig(nodeObjects) {
                 { "geoip": "cn", "outbound": "direct" }
                 
             ],
-            "default_outbound": "manual-select"
+            "final": "manual-select"
         }
     };
     
