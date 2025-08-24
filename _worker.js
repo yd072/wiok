5f72fdcc6b6981d6bcbd72ed65d3c77a235869bb
yd072 <luiquirong@gmail.com>
2025-08-25 01:32:15 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2732,9 +2732,9 @@ external-controller: 127.0.0.1:9090
 dns:
   enable: true
   listen: 0.0.0.0:53
-  default-nameserver: [223.5.5.5, 119.29.29.29, 8.8.8.8]
-  nameserver: ['https://dns.alidns.com/dns-query', 'https://doh.pub/dns-query']
-  fallback: ['https://dns.google/dns-query', 'https://cloudflare-dns.com/dns-query']
+  default-nameserver: [223.5.5.5, 8.8.8.8]
+  nameserver: ['https://dns.alidns.com/dns-query']
+  fallback: ['https://dns.google/dns-query']
   
 proxies:
 ${proxiesYaml}
